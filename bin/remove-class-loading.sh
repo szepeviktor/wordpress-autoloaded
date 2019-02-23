c7767cd7ec02ba6829d9b8540d1490ec07d0f748
Viktor Szépe <viktor@szepe.net>
2019-02-23 15:10:30 +0000

    Restructure script

--- bin/remove-class-loading.sh
+++ bin/remove-class-loading.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#
+# 1. List all files Composer finds a class in
+# 2. Remove "wp-includes/" from path
+# 3. Find all references to these files
+# 4. Comment out all references
+
+
+Get_class_files()
+{
+    sed -n -e "s#.* . '/app/wordpress/\(.*\)',#\1#p" ../../vendor/composer/autoload_classmap.php | sort -u \
+        | sed -e 's#^wp-includes/##' \
+        | xargs -I "%" grep -r -E -x -n "\s*(require|include)(_once)?\(?\s*ABSPATH(\s*\.\s*WPINC)?\s*\.\s*'/?%'\s*\)?;.*" | cut -d ":" -f 1,2
+}
 
 set -e
 
@@ -8,11 +21,11 @@ cd app/wordpress/
 
 test -r ../../vendor/composer/autoload_classmap.php
 
-sed -n -e "s#.* . '/app/wordpress/\(.*\)',#\1#p" ../../vendor/composer/autoload_classmap.php | uniq \
-    | xargs -I "%" grep -r -E -x -n "\s*(require|include)(_once)?\(?\s*ABSPATH\s*\.\s*'/?%'\s*\)?;.*" | cut -d ":" -f 1,2 \
-    | while read -r FILE_LINE; do
-        echo "$FILE_LINE ..."
-        sed -e "${FILE_LINE#*:}"'s|.*|// AUTOLOAD &|' -i "${FILE_LINE%:*}"
-    done
+CLASS_FILES="$(Get_class_files)"
+
+while read -r FILE_LINE; do
+    echo "$FILE_LINE ..."
+    sed -e "${FILE_LINE#*:}"'s#.*#// AUTOLOAD &#' -i "${FILE_LINE%:*}"
+done <<<"$CLASS_FILES"
 
 echo "OK."
